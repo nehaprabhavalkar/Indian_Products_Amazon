3b2e47a2f7ebd8351c869cc388049947cc0cb775
nehaprabhavalkar <nehaprabhavalkar47@gmail.com>
2022-02-01 21:31:56 +0530

    cleaned the repo

--- README.md
+++ README.md
@@ -2,4 +2,3 @@
 
 Web Scraping Indian Products reviews from Amazon using BeautifulSoup
 
-Dataset link : https://www.kaggle.com/nehaprabhavalkar/indian-products-on-amazon
---
+++ code/scraping.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+import re
+import datetime
+import warnings
+warnings.filterwarnings('ignore')
+
+
+DATA_PATH = '../data/'
+
+
+if __name__ == '__main__':
+    pass 
+
